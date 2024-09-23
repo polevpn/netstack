0c19692d6d3a46a0fb1528331cbc29a507911d8b
dev123 <dev123@gmail.com>
2024-09-23 20:17:45 +0800

    remove asm

--- tcpip/time_unsafe.go
+++ tcpip/time_unsafe.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build go1.9
 // +build go1.9
-// +build !go1.18
 
 // Check go:linkname function signatures when updating Go version.
 
